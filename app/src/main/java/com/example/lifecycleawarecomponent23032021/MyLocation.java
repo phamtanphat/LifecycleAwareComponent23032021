a3b0f295e83666c1934a205636f4379692891de3
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-12 20:50:39 +0700

    get location

--- app/src/main/java/com/example/lifecycleawarecomponent23032021/MyLocation.java
+++ app/src/main/java/com/example/lifecycleawarecomponent23032021/MyLocation.java
@@ -1,10 +1,12 @@
 package com.example.lifecycleawarecomponent23032021;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Handler;
 import android.os.Handler.Callback;
 
 import androidx.core.app.ActivityCompat;
@@ -16,29 +18,45 @@ public class MyLocation {
     Location gps_loc;
     Location network_loc;
     Location final_loc;
-    double longitude;
-    double latitude;
+    double longitudeNew;
+    double latitudeNew;
     LocationManager locationManager;
-
+    Handler mHandler;
 
     public MyLocation(Context mContext, OnListenLocation mOnListenLocation) {
         this.mContext = mContext;
         this.mOnListenLocation = mOnListenLocation;
         locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        mHandler = new Handler();
     }
 
-    public void startListenLocation() {
-        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
 
+    public void startListenLocation() {
+        mHandler.postDelayed(runnableGetLocation, 1000);
+    }
+
+    @SuppressLint("MissingPermission")
+    private Runnable runnableGetLocation = new Runnable() {
+        @Override
+        public void run() {
+            gps_loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            network_loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if (gps_loc != null) {
+                final_loc = gps_loc;
+                latitudeNew = final_loc.getLatitude();
+                longitudeNew = final_loc.getLongitude();
+            } else if (network_loc != null) {
+                final_loc = network_loc;
+                latitudeNew = final_loc.getLatitude();
+                longitudeNew = final_loc.getLongitude();
+            } else {
+                latitudeNew = 0.0;
+                longitudeNew = 0.0;
+            }
+            mHandler.postDelayed(this,1000);
+        }
+    };
+    public void stopGeLocation(){
+        mHandler.removeCallbacks(runnableGetLocation);
     }
 }
